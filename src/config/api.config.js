7aae453a86fb44ac2787e59904a33f428bccbdf6
German Ordoñez <100479630+gaordonezh@users.noreply.github.com>
2024-03-09 11:18:56 -0500

    Update api.config.js

--- src/config/api.config.js
+++ src/config/api.config.js
@@ -1,2 +1,3 @@
-const API_HOSPITAL = "https://nodejs-hospital-production.up.railway.app/api";
+// const API_HOSPITAL = "https://nodejs-hospital-production.up.railway.app/api";
+const API_HOSPITAL = "https://clean-worm-tweed-jacket.cyclic.app/api";
 export default API_HOSPITAL;
